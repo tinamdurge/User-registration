f72a6f10d38deecac3d68bdfa5fbb1ba339993fe
Tinam Durge <tinamdurge02@gmail.com>
2023-12-11 18:28:48 +0530

    Added sample email ids

--- src/TestSampleEmailIds.java
+++ src/TestSampleEmailIds.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class TestSampleEmailIds {
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class TestSampleEmailIds {
+    static String[] validEmails = {"abc@yahoo.com", "abc-100@yahoo.com", "abc.100@yahoo.com",
+            "abc111@abc.com", "abc-100@abc.net", "abc.100@abc.com.au",
+            "abc@1.com", "abc@gmail.com.com", "abc+100@gmail.com", "abc", "abc@.com.my","abc123@gmail.a", "abc123@.com",
+            "abc123@.com.com", ".abc@abc.com", "abc()*@gmail.com",
+            "abc@%*.com", "abc..2002@gmail.com", "abc.@gmail.com",
+            "abc@abc@gmail.com", "abc@gmail.com.1a", "abc@gmail.com.aa.au"};
+
+    public void testForValidEmails() {
+        for (int i = 0; i < validEmails.length; i++) {
+            Pattern pattern = Pattern.compile("^[a-zA-Z-9]+([._+-]*[0-9A-Za-z]+)*@[a-zA-Z0-9]+.[a-zA-Z]{2,4}([.][a-z]{2,4})?$");
+            Matcher matcher = pattern.matcher(validEmails[i]);
+            if (matcher.matches()) {
+                System.out.println(validEmails[i]+" : Valid");
+            } else {
+                System.out.println(validEmails[i]+" : Invalid");
+            }
+        }
+        System.out.println(" ");
+    }
 }
