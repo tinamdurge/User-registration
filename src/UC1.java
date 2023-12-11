e61c37cc19e85369191b573066b2f28558cc95c6
Tinam Durge <tinamdurge02@gmail.com>
2023-12-11 18:14:58 +0530

    Enter a valid first name

--- src/UC1.java
+++ src/UC1.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class UC1 {
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class UC1 {
+
+    public void firstName(String str) {
+        int flag = 0;
+        Pattern pattern = Pattern.compile("^[A-Z][a-z]{3}$");
+        Matcher matcher = pattern.matcher(str);
+        while (matcher.find()) {
+            flag = 1;
+        }
+        if (flag == 1) {
+            System.out.println("Valid First Name");
+        } else {
+            System.out.println("Invalid First Name");
+        }
+    }
 }
