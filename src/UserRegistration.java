baa518109e2b334ce9dba55d9773e38492b06084
Tinam Durge <tinamdurge02@gmail.com>
2023-12-11 18:25:32 +0530

    Added user registration file

--- src/UserRegistration.java
+++ src/UserRegistration.java
@@ -1,2 +1,41 @@
-package PACKAGE_NAME;public class UserRegistration {
+import java.util.Scanner;
+
+public class UserRegistration {
+    Scanner scanner = new Scanner(System.in);
+    UserDetails userDetails = new UserDetails();
+
+    public void userFirstName() {
+        System.out.println("Enter Your First Name : ");
+        String firstName = scanner.next();
+        userDetails.firstName(firstName);
+    }
+
+    public void userLastName() {
+        System.out.println("Enter Your Last Name : ");
+        String lastName = scanner.next();
+        userDetails.lastName(lastName);
+    }
+
+    public void userEmailId() {
+        System.out.println("Enter Your EmailId : ");
+        String emailId = scanner.next();
+        userDetails.email(emailId);
+    }
+
+    public void userMobileNumber() {
+        System.out.println("Enter your MobileNumber");
+        String phoneNumber = scanner.next();
+        userDetails.phoneNumber(phoneNumber);
+    }
+
+    public void userPassword() {
+        System.out.println("Enter your MobileNumber");
+        String password = scanner.next();
+        userDetails.password(password);
+    }
+
+    public void sampleEmailIds() {
+        TestSampleEmailIds testSampleEmailIds = new TestSampleEmailIds();
+        testSampleEmailIds.testForValidEmails();
+    }
 }
