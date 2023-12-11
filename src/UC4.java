d54c4edba13bf93934f0785f4cfdaf3d463c18d7
Tinam Durge <tinamdurge02@gmail.com>
2023-12-11 18:17:22 +0530

    Follow pre-defined mobile format

--- src/UC4.java
+++ src/UC4.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class UC4 {
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class UC4 {
+    public void validate(String str) {
+        int flag = 0;
+        Pattern pattern = Pattern.compile("^(\\+?\\d{1,2}?)?(\\d{10})$");
+        Matcher matcher = pattern.matcher(str);
+        while (matcher.find()) {
+            flag = 1;
+        }
+        if (flag == 1) {
+            System.out.println("valid phone number");
+        } else {
+            System.out.println("invalid phone number");
+        }
+
+    }
 }
