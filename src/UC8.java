3fc771facc245af5414573db84202dcc16abacd4
Tinam Durge <tinamdurge02@gmail.com>
2023-12-11 18:20:09 +0530

    Has exactly 1 special character

--- src/UC8.java
+++ src/UC8.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class UC8 {
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class UC8 {
+    public void validate(String str) {
+        int flag = 0;
+        Pattern pattern = Pattern.compile("^(?=.*[A-Z])(?=.*[0-9])(?=.*[@$!%*#?&])([a-zA-Z0-9@$!%*#?&]){8,}$");
+        Matcher matcher = pattern.matcher(str);
+        while (matcher.find()) {
+            flag = 1;
+        }
+        if (flag == 1) {
+            System.out.println("valid password");
+        } else {
+            System.out.println("invalid password");
+        }
+    }
 }
