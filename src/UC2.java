da761db11f7b4b79cb86128228d912a2bcca8197
Tinam Durge <tinamdurge02@gmail.com>
2023-12-11 18:15:32 +0530

    Enter a valid last name

--- src/UC2.java
+++ src/UC2.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class UC2 {
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class UC2 {
+    public void lastName(String str) {
+        int flag = 0;
+        Pattern pattern = Pattern.compile("^[A-Z][a-z]{2}");
+        Matcher matcher = pattern.matcher(str);
+        while (matcher.find()) {
+            flag = 1;
+        }
+        if (flag == 1) {
+            System.out.println("valid last Name");
+        } else {
+            System.out.println("invalid last Name");
+        }
+
+    }
 }
