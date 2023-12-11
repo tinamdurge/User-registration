02a25f852aff84a723af8dd52a067a864ed1064d
Tinam Durge <tinamdurge02@gmail.com>
2023-12-11 18:16:08 +0530

    Enter a valid email

--- src/UC3.java
+++ src/UC3.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class UC3 {
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class UC3 {
+    public void email(String str) {
+        int flag = 0;
+        Pattern pattern = Pattern.compile("^abc.[A-Za-z]+@bl.co");
+        Matcher matcher = pattern.matcher(str);
+        while (matcher.find()) {
+            flag = 1;
+        }
+        if (flag == 1) {
+            System.out.println("valid email");
+        } else {
+            System.out.println("invalid email");
+        }
+
+    }
 }
