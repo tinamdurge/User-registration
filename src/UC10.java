ccaf4925ccfa2635c50fb58222556968176986d2
Tinam Durge <tinamdurge02@gmail.com>
2023-12-11 18:21:11 +0530

    JUnit to validate entries

--- src/UC10.java
+++ src/UC10.java
@@ -1,2 +1,43 @@
-package PACKAGE_NAME;public class UC10 {
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class UC10 {
+    public boolean isValid(Matcher matcher){
+        if (matcher.matches()) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    public boolean validateFirstName(String firstName){
+        Pattern namePattern = Pattern.compile("^[A-Z]{1}[a-z]{3,}$");
+        Matcher firstNameMatcher = namePattern.matcher(firstName);
+        return isValid(firstNameMatcher);
+    }
+
+    public boolean validateLastName(String lastName){
+        Pattern namePattern = Pattern.compile("^[A-Z]{1}[a-z]{3,}$");
+        Matcher lastNameMatcher = namePattern.matcher(lastName);
+        return isValid(lastNameMatcher);
+    }
+
+    public boolean validateEmail(String email){
+        Pattern sampleEmailPattern = Pattern.compile("^[0-9a-zA-Z]+([+.-]([a-z0-9A-Z]+))*[@][a-zA-Z0-9]+[.][a-z]{2,4}[,]?([.][a-z]{2,4})?$");
+        Matcher sampleEmailMatcher = sampleEmailPattern.matcher(email);
+        return isValid(sampleEmailMatcher);
+    }
+
+    public boolean validatePhoneNumber(String phoneNumber) {
+        Pattern phoneNoPattern = Pattern.compile("^[0-9]{2}[0-9]{10}$");
+        Matcher phoneNoMatcher = phoneNoPattern.matcher(phoneNumber);
+        return isValid(phoneNoMatcher);
+    }
+
+    public boolean validatePassword(String password){
+        Pattern passwordPattern = Pattern.compile("^(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*?])[!@#$%^&*?A-Z+a-z0-9]{8,}$");
+        Matcher passwordMatcher = passwordPattern.matcher(password);
+        return isValid(passwordMatcher);
+    }
 }
