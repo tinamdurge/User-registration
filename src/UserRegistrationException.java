9f4827947aeee610c9ee8cbd98948669493f1c30
Tinam Durge <tinamdurge02@gmail.com>
2023-12-11 18:26:36 +0530

    Added user registration exception file

--- src/UserRegistrationException.java
+++ src/UserRegistrationException.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class UserRegistrationException {
+public class UserRegistrationException extends Exception {
+    public String message;
+    public ExceptionType exceptionType;
+
+    public enum ExceptionType {
+        NULL, EMPTY, INVALID_FIRSTNAME, INVALID_LASTNAME, INVALID_MOBILE_NUMBER, INVALID_EMAIL, INVALID_PASSWORD;
+    }
+
+    UserRegistrationException(String message, ExceptionType exceptionType) {
+        this.message = message;
+        this.exceptionType = exceptionType;
+    }
+
+
 }
