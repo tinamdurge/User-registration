857a3e7b8bef8bb08e2b9800edeaf5216b5751d6
Tinam Durge <tinamdurge02@gmail.com>
2023-12-11 18:18:45 +0530

    Have atleast 1 uppercase

--- src/UC6.java
+++ src/UC6.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class UC6 {
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class UC6 {
+    public void validate(String str) {
+        int flag = 0;
+        Pattern pattern = Pattern.compile("^(?=.*[A-Z])([a-zA-Z0-9-_.]){8,}$");
+        Matcher matcher = pattern.matcher(str);
+        while (matcher.find()) {
+            flag = 1;
+        }
+        if (flag == 1) {
+            System.out.println("valid password");
+        } else {
+            System.out.println("invalid password");
+        }
+    }
 }
